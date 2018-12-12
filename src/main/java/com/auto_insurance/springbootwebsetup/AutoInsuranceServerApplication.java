fb39d9391428f67e727576fb7c8b291a78732007
Zack Blaylock <zackblaylock@gmail.com>
2018-12-12 14:31:48 -0500

    Initial Commit

---
+++ src/main/java/com/auto_insurance/springbootwebsetup/AutoInsuranceServerApplication.java
@@ -0,0 +1,14 @@
+package com.auto_insurance.springbootwebsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoInsuranceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoInsuranceServerApplication.class, args);
+	}
+
+}
+
